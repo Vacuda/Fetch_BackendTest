e6fff54d00d848652855b1dc2ff41f9f14075509
Vacuda <adamjsikora@gmail.com>
2023-09-08 14:50:43 -0500

    Added Receipt And Item Structs

--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+/* STRUCTS */
+type Receipt struct{
+	Retailer		string
+	PurchaseDate	time.Time
+	PurchaseTime	time.Time
+	Total			float32
+	Items			[]Item
+}
+
+type Item struct{
+	ShortDescription	string
+	Price				float32
+}
+
+/* MAIN */
 
 func main(){
 	var message string = "Hello, World."
 	fmt.Println(message)
-
+	time.Now()
 }
+
+/* UTILITIES */
+
