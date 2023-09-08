279864ed01e0f59acbc88a68d596c7c629efe1c6
Vacuda <adamjsikora@gmail.com>
2023-09-08 15:43:04 -0500

    Marshalled Json Object Into Struct

--- main.go
+++ main.go
@@ -3,28 +3,47 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"encoding/json"
+	"log"
 )
 
 /* STRUCTS */
-type Receipt struct{
-	Retailer		string
-	PurchaseDate	time.Time
-	PurchaseTime	time.Time
-	Total			float32
-	Items			[]Item
-}
-
 type Item struct{
 	ShortDescription	string
 	Price				float32
 }
 
+type Receipt struct{
+	Retailer 		string 		`json:"retailer"`	
+	PurchaseDate	time.Time	`json:"purchaseDate"`	
+	PurchaseTime	time.Time	`json:"purchaseTime"`	
+	Total			float32		`json:"total"`	
+	Items			[]Item		`json:"items"`	
+}
+
+
+/* DATA */
+
+var Receipts []Receipt = make([]Receipt, 0)
+
 /* MAIN */
 
 func main(){
-	var message string = "Hello, World."
+
+	var this_receipt Receipt
+
+	lava, err := os.ReadFile("examples/morning-receipt.json")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	json.Unmarshal(lava, &this_receipt)
+
+	var message string = this_receipt.Retailer
 	fmt.Println(message)
-	time.Now()
 }
 
 /* UTILITIES */
